eba4b20fbc2ecee37002df8b420779dc1fba67b9
Ross Cannizzaro <4198648+rcannizzaro@users.noreply.github.com>
2020-07-09 12:51:39 +0930

    Add SSHD

--- build/root/install.sh
+++ build/root/install.sh
@@ -35,7 +35,7 @@ fi
 ####
 
 # define pacman packages
-pacman_packages="ttf-dejavu xorg-fonts-misc terminus-font ttf-dejavu xfce4-terminal tint2 xorg-server-xvfb tigervnc openbox obconf python2-xdg lxappearance xcompmgr cantarell-fonts python-pip python-numpy feh"
+pacman_packages="ttf-dejavu xorg-fonts-misc terminus-font ttf-dejavu xfce4-terminal tint2 xorg-server-xvfb tigervnc openbox obconf python2-xdg lxappearance xcompmgr cantarell-fonts python-pip python-numpy sshd feh"
 
 # install compiled packages using pacman
 if [[ ! -z "${pacman_packages}" ]]; then
