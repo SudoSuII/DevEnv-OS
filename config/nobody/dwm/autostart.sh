3ab754fd2d198c1f54a3b6357c4ba5f59032995e
Ross Cannizzaro <4198648+rcannizzaro@users.noreply.github.com>
2020-07-12 08:52:51 +0930

    Java DWM workarounds.

--- config/nobody/dwm/autostart.sh
+++ config/nobody/dwm/autostart.sh
@@ -8,6 +8,12 @@ xcompmgr &
 
 feh --bg-center "/home/nobody/.themes/wallpaper.png" &
 
+# Java fix for DWM
+# https://wiki.gentoo.org/wiki/Dwm#Blank_.28grey.29_windows_of_Java_applications_.28such_as_netbeans.29
+
+wmname LG3D &
+xsetroot -solid black &
+
 # Launch DMenu
 
 dmenu_run &
