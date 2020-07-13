3608effdd9f70bd3410c5a75c528f027fd6768f2
Ross Cannizzaro <4198648+rcannizzaro@users.noreply.github.com>
2020-07-13 12:52:11 +0930

    Re-add dmenu_run for now.

--- config/nobody/dwm/autostart.sh
+++ config/nobody/dwm/autostart.sh
@@ -12,3 +12,8 @@ nitrogen --restore &
 # https://wiki.gentoo.org/wiki/Dwm#Blank_.28grey.29_windows_of_Java_applications_.28such_as_netbeans.29
 
 wmname LG3D &
+xsetroot -solid black &
+
+# Launch DMenu
+
+dmenu_run &
